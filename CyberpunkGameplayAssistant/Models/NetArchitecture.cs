79a95f7fc02d00b7c0ad69dc715a77ba77a4327d
Jorden Finne <jorden.finne@gmail.com>
2022-05-11 09:17:58 -0500

    UI in place for NET Architecture gneeration, backend code pending

---
+++ CyberpunkGameplayAssistant/Models/NetArchitecture.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CyberpunkGameplayAssistant.Models
+{
+    public class NetArchitecture : BaseModel
+    {
+        // Constructors
+        public NetArchitecture()
+        {
+
+        }
+        public NetArchitecture(string name, string difficulty)
+        {
+
+        }
+
+        // Databound Properties
+        private string _Name;
+        public string Name
+        {
+            get => _Name;
+            set => SetAndNotify(ref _Name, value);
+        }
+        private string _Difficulty;
+        public string Difficulty
+        {
+            get => _Difficulty;
+            set => SetAndNotify(ref _Difficulty, value);
+        }
+        private ObservableCollection<NetFloor> _Floors;
+        public ObservableCollection<NetFloor> Floors
+        {
+            get => _Floors;
+            set => SetAndNotify(ref _Floors, value);
+        }
+
+    }
+}
---
+++ CyberpunkGameplayAssistant/Models/NetFloor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CyberpunkGameplayAssistant.Models
+{
+    [Serializable]
+    public class NetFloor : BaseModel
+    {
+        // Constructors
+        public NetFloor()
+        {
+
+        }
+        public NetFloor(int level, string description)
+        {
+            Level = level;
+            Description = description;
+        }
+
+        // Databound Properties
+        private int _Level;
+        public int Level
+        {
+            get => _Level;
+            set => SetAndNotify(ref _Level, value);
+        }
+        private string _Description;
+        public string Description
+        {
+            get => _Description;
+            set => SetAndNotify(ref _Description, value);
+        }
+        private string _Notes;
+        public string Notes
+        {
+            get => _Notes;
+            set => SetAndNotify(ref _Notes, value);
+        }
+
+    }
+}
